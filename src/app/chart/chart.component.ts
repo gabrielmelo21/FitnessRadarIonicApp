68f5bddac39c19544ed2d977e4bef12446195d0a
Gabriel de Melo <89091392+gabrielmelo21@users.noreply.github.com>
2023-09-30 13:34:27 -0300

    Chart Configs

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Chart} from 'chart.js';
+import {MainApiService} from "../services/main-api.service";
 
 
 @Component({
@@ -7,5 +8,43 @@ import {Chart} from 'chart.js';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss'],
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+  config: any;
+  labels: any = ['27/09', '28/09', '29/09', '30/09', '01/10'];
+  dates: any = [23, 25, 14, 17, 25];
+  constructor(private mainAPI: MainApiService) {
+
+    if (this.labels.length > 7 && this.dates.length > 7 ){
+      // retire o primeiro dado para manter a constancia do grafico
+    }
+
+
+  }
+
+  ngOnInit(): void {
+    // Este é o lugar onde o gráfico será criado, após o componente ser inicializado.
+    this.createLineChart();
+  }
+
+  createLineChart() {
+    const labels = this.labels;
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'Deficit Calórico',
+        data: this.dates,
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    };
+
+    this.config  = {
+      type: 'line',
+      data: data,
+    };
+
+    new Chart('myChart', this.config);
+  }
+
 }
